1695171452180524cf231429d0e536cfad7ec185
Niraj <58387592+nirajsingh26@users.noreply.github.com>
2025-02-18 22:51:54 +0530

    Optimized code base , added comments to specific function and added readme file (#9)
    Co-authored-by: Niraj <58387592+rebelnato@users.noreply.github.com>

--- crud/staticCRUD.go
+++ crud/staticCRUD.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/rebelnato/gosqlite3/db/connection"
+	"github.com/rebelnato/gosqlite3"
 )
 
 type UserData struct {
@@ -15,22 +15,44 @@ type UserData struct {
 	passwordFromDb string
 }
 
-// SQLlite 3 db related functions
+/*
+ Below function will be used to perform user data insertion into associated db
+ This can be beneficial when accompanied with a create user rest API
+
+ Accepts user name and hashed password in ideal scenario which should be in string format
+ Returns error based on execution status
+*/
+
 func InsertData(username, password string) error {
-	db, _ := connection.ConnectToSQLiteDB()
+	db, dbError := gosqlite3.ReadDbConfig()
+	if dbError != nil {
+		fmt.Println("Failed to read config details , stoping execution now")
+		return dbError
+	}
 	defer db.Close()
+
 	_, err := db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", username, password)
 	return err
 }
 
+/*
+ Below function will be used to fetch details related to a specific user
+ This can be beneficial when accompanied with a user credential validation rest API
+
+ Accepts user name in string format
+ Returns data associated with user ( id , username and password )
+*/
+
 func QueryData(username string) (id int, user string, password string, err error) {
-	db, _ := connection.ConnectToSQLiteDB()
+	db, dbError := gosqlite3.ReadDbConfig()
+	if dbError != nil {
+		fmt.Println("Failed to read config details , stoping execution now")
+		return 0, "", "", dbError
+	}
 	defer db.Close()
 
-	// Declaring var to store password temporarly as part of db query task
-	var user1 UserData
+	var user1 UserData // Declaring var to store password temporarly as part of db query task
 
-	// Fetch user password from db
 	userPassFromDB := db.QueryRow("SELECT id,username,password FROM users WHERE username = ?", username).Scan(&user1.id, &user1.username, &user1.passwordFromDb)
 	if userPassFromDB != nil {
 		if userPassFromDB == sql.ErrNoRows {
@@ -42,9 +64,21 @@ func QueryData(username string) (id int, user string, password string, err error
 	return user1.id, user1.username, user1.passwordFromDb, userPassFromDB
 }
 
+/*
+Below function will be used to fetch list of all users available in users table
+This can be beneficial for admin user where in they want to get list of all users
+
+Doesn't accepts any argument
+Returns list of users available in "users" table and error based on execution status
+*/
 func QueryUserList() ([]string, error) {
-	db, _ := connection.ConnectToSQLiteDB()
+	db, dbError := gosqlite3.ReadDbConfig()
+	if dbError != nil {
+		fmt.Println("Failed to read config details , stoping execution now")
+		return nil, dbError
+	}
 	defer db.Close()
+
 	rows, err := db.Query("SELECT username FROM users")
 	if err != nil {
 		return nil, err
@@ -62,28 +96,66 @@ func QueryUserList() ([]string, error) {
 	return users, nil
 }
 
+/*
+ Below function will be used for updating the user name of an existing user
+ This can be beneficial when user wants to update their user name
+
+ Ideally this should be paired with a user validation function for user identification
+ Accepts old and new user name
+ Returns error based on function execution status
+*/
+
 func UpdateUsername(oldUsername, newUsername string) error {
-	db, _ := connection.ConnectToSQLiteDB()
+	db, dbError := gosqlite3.ReadDbConfig()
+	if dbError != nil {
+		fmt.Println("Failed to read config details , stoping execution now")
+		return dbError
+	}
 	defer db.Close()
 	id, _, _, _ := QueryData(oldUsername)
 	_, err := db.Exec("UPDATE users SET username = ? WHERE id = ?", newUsername, id)
 	return err
 }
 
+/*
+ Below function will be used for updating the password for existing user
+ This can be beneficial when user wants to update their password
+
+ Ideally this should be paired with a user validation function for user identification
+ Accepts user name and new password
+ Returns error based on function execution status
+*/
+
 func UpdatePassword(username, newPassword string) error {
-	db, _ := connection.ConnectToSQLiteDB()
+	db, dbError := gosqlite3.ReadDbConfig()
+	if dbError != nil {
+		fmt.Println("Failed to read config details , stoping execution now")
+		return dbError
+	}
 	defer db.Close()
 	_, err := db.Exec("UPDATE users SET password = ? WHERE username = ?", newPassword, username)
 	return err
 }
 
+/*
+Below function will be used deleting a specific user from users table
+This can be beneficial when user wants to delete his/her account
+
+Internally performs user creds validation
+Accepts username and password
+Returns error based on function execution status
+*/
+
 func DeleteUser(username, password string) error {
-	db, _ := connection.ConnectToSQLiteDB()
+	db, dbError := gosqlite3.ReadDbConfig()
+	if dbError != nil {
+		fmt.Println("Failed to read config details , stoping execution now")
+		return dbError
+	}
 	defer db.Close()
 	_, usernameFromDb, passwordFromDb, err := QueryData(username)
 	if err != nil {
 		fmt.Printf("Failed to fetch user data from db due to error message %q", err)
-		// fmt.Printf("Reinitiating the example test flow due to failure while fetching the user data .")
 		return err
 	} else {
 		if username == usernameFromDb && password == passwordFromDb {
