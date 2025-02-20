5462b466e47e36f15a03bec59bc6dd57739ce5d7
Niraj <58387592+rebelnato@users.noreply.github.com>
2025-02-21 01:40:27 +0530

    Replacing fmt print with log so that data can be pushed to logs

--- entry.go
+++ entry.go
@@ -2,7 +2,7 @@ package gosqlite3
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -53,19 +53,19 @@ var dbConfig Config
 func ReadDbConfig() (*sql.DB, error) {
 	data, err := os.ReadFile("./db/config/config.yml")
 	if err != nil {
-		fmt.Println("Error reading YAML file:", err)
+		log.Println("Error reading YAML file:", err)
 		return nil, err
 	}
 
 	err = yaml.Unmarshal(data, &dbConfig)
 	if err != nil {
-		fmt.Println("Error parsing YAML:", err)
+		log.Println("Error parsing YAML:", err)
 		return nil, err
 	}
 
 	db, err := ConnectToSQLiteDB(dbConfig.DatabaseConfig.DbName, dbConfig.DatabaseConfig.DbPath)
 	if err != nil {
-		fmt.Println("Connection to database failed")
+		log.Println("Connection to database failed")
 		return nil, err
 	}
 	return db, err
@@ -92,11 +92,11 @@ func ConnectToSQLiteDB(dbName, dbPath string) (*sql.DB, error) {
 		return nil, err
 	}
 
-	fmt.Println("Connected to SQLite Database!")
+	log.Println("Connected to SQLite Database!")
 
 	_, createTableError := db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, password TEXT);")
 	if createTableError != nil {
-		fmt.Printf("Failed to create users table eventhough it doesn't exists with error message %q", createTableError)
+		log.Printf("Failed to create users table eventhough it doesn't exists with error message %q", createTableError)
 		return db, createTableError
 	}
 
